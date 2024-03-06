7ba32ca018062ca24a29bfdc2188621a92d1c98d
Denis B <sinoptikgad@mail.ru>
2024-03-07 02:01:23 +0300

    fifth step done

---
+++ __tests__/genDiff.test.js
@@ -0,0 +1,18 @@
+import fs from 'fs';
+import { test, expect } from '@jest/globals';
+import { fileURLToPath } from 'url';
+import path from 'path';
+import genDiff from '../index.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
+
+test.each(['json', 'yaml'])('genDiff', (extension) => {
+  const filePath1 = getFixturePath(`file1.${extension}`);
+  const filePath2 = getFixturePath(`file2.${extension}`);
+  const expected = readFile('expected_file.txt');
+  const actual = genDiff(filePath1, filePath2);
+  expect(actual).toEqual(expected);
+});
--- src/genDiff.js
+++ src/genDiff.js
@@ -1,15 +1,15 @@
 import _ from 'lodash';
-import parsers from "./parsers.js";
+import parsers from './parsers.js';
 
 const resultArrayOfKeys = (obj1, obj2) => {
-    const arr1 = Object.keys(obj1);
-    const arr2 = Object.keys(obj2);
-    const concatArray = [...arr1, ...arr2];
-    const newArr = new Set(concatArray);
-    const result = Array.from(newArr);
-    return _.sortBy(result);
-  };
-  
+  const arr1 = Object.keys(obj1);
+  const arr2 = Object.keys(obj2);
+  const concatArray = [...arr1, ...arr2];
+  const newArr = new Set(concatArray);
+  const result = Array.from(newArr);
+  return _.sortBy(result);
+};
+
 const genDiff = (filepath1, filepath2) => {
   const obj1 = parsers(filepath1);
   const obj2 = parsers(filepath2);
@@ -27,14 +27,14 @@ const genDiff = (filepath1, filepath2) => {
       }
     }
     if (!hasPropertyObj1) {
-        accumulate += `\n  + ${key} : ${obj2[key]}`;
+      accumulate += `\n  + ${key} : ${obj2[key]}`;
     }
     if (!hasPropertyObj2) {
-        accumulate += `\n  - ${key} : ${obj1[key]}`;
+      accumulate += `\n  - ${key} : ${obj1[key]}`;
     }
-      return accumulate;
-    }, '');
-    return `{${result}\n}`;
-  };
-  
+    return accumulate;
+  }, '');
+  return `{${result}\n}`;
+};
+
 export default genDiff;
